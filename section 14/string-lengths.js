fba0be0e9d30c87f34be694bb1d81018afa675cf
lolesterrr <kajjabwangulester@gmail.com>
2026-02-17 13:15:07 +0300

    string lengths

---
+++ section 14/string-lengths.js
@@ -0,0 +1,8 @@
+var name = "Lester"; 
+name.length // use the length function
+
+
+//challenge one 
+
+var userText = prompt("write something")
+alert("You have written" + " " + userText.length + " " +"characters" + ". You have " + (140-userText.length) + " " + "characters left." )
---
+++ section 14/strings.js
@@ -0,0 +1,8 @@
+alert("hello"+ " " + "world") //concatenation
+
+
+//challenge one 
+var message = "Hello";
+var name = "Lester";
+
+alert(message + " " + name )

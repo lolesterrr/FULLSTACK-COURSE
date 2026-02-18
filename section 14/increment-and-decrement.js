34b0eea172bc1ae69449102741dccb512246d398
lolesterrr <kajjabwangulester@gmail.com>
2026-02-18 16:06:17 +0300

    functions v2

--- section 14/increment-and-decrement.js
+++ section 14/increment-and-decrement.js
@@ -3,4 +3,4 @@ x++ ; // shorthand for x = x + 1;
 x-- ; // shorthand for x = x - 1; 
 
 x += 2 ; // shorthand for x = x + 2 ;
-x += y ; // can be used on variables too
+x += y ; // can be used on variables too and works for other operators like * and /

95b0ec6f7abf9715b120c463014a295aabd94e4a
lolesterrr <kajjabwangulester@gmail.com>
2026-02-20 10:24:33 +0300

    conditionals

---
+++ section 14/conditionals.js
@@ -0,0 +1,10 @@
+
+var x = prompt("enter a number for X")
+var y = prompt("enter a number for Y")
+//this is the format for the if-else conditional
+if (x > y){     
+    alert("X is greater than y")
+
+} else{
+    alert("Y is greater than x")
+};

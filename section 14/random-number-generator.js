6ebe2cf7ba936eac8bbda35d383ab10cf3180ccb
lolesterrr <kajjabwangulester@gmail.com>
2026-02-20 10:03:43 +0300

    random number generator v2

--- section 14/random-number-generator.js
+++ section 14/random-number-generator.js
@@ -5,4 +5,4 @@ var lover2Name = prompt("What is your crush's name?")
 var lovePercentage = Math.random();
  lovePercentage *= 100
  lovePercentage = Math.floor(lovePercentage) + 1
- alert("Your Percentage Match is " + lovePercentage + "% , Congratulations")
+ alert("Your Percentage Match with " + lover2Name " is " + lovePercentage + "% , Congratulations")

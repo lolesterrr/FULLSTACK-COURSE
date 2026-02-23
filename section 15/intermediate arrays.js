03275283297f4a3f196aafe370f50b3578bd5dca
lolesterrr <kajjabwangulester@gmail.com>
2026-02-23 13:28:41 +0300

    intermediate arrays v1

---
+++ section 15/intermediate arrays.js
@@ -0,0 +1,28 @@
+
+
+// personal solution to challenge one
+var output = [];
+
+function fizzbuzz(){
+    output.push(output.length + 1 )
+
+
+    console.log(output)
+}
+
+fizzbuzz();
+fizzbuzz();
+fizzbuzz();
+
+// instructor solution to challenge 1
+
+var output = [];
+var count = 1;
+
+ function fizzbuzz(){
+
+    output.push(count);
+    count++;
+
+    console.log(output);
+ }

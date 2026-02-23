ec5a74bfe87a8aadc624720be89f8e25f8729123
lolesterrr <kajjabwangulester@gmail.com>
2026-02-23 13:01:02 +0300

    arrays

---
+++ collection.js
@@ -0,0 +1,14 @@
+var guestList = ["Joseph", "Lester", "Kajja", "Mae"] // create array
+
+
+//challenge 1
+
+var guest = prompt("What is your name?")
+ if (guestList.includes(guest)){
+    alert("Welcome, " + guest)
+
+
+ }else{
+    alert("You're not on the guest list")
+ }
+

800ad1d636490a3e1085b4439f2e52d771ec8ecd
lolesterrr <kajjabwangulester@gmail.com>
2026-02-28 13:37:18 +0300

    challenge 7

--- section 17/Dicee+Challenge+-+Starting+Files/index.js
+++ section 17/Dicee+Challenge+-+Starting+Files/index.js
@@ -6,3 +6,9 @@ var randomImageSource = "images/" + randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
+image1.setAttribute("src", randomImageSource); 
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource);
+

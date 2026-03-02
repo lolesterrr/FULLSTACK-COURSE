34e74a33a4c0aa9b25071d3f7e48c794ad1e4bd8
lolesterrr <kajjabwangulester@gmail.com>
2026-03-02 13:43:46 +0300

    challenge 1

--- section 18/Drum Kit Starting Files/index.js
+++ section 18/Drum Kit Starting Files/index.js
@@ -1,9 +1,12 @@
 
-var numberOfDrumButtons = document.querySelectorAll)(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
     for (i = 0; i<numberOfDrumButtons; i++){
 
          document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-            alert("i got clicked!")
+
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+
          });
 
     }

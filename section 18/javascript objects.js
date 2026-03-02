fabe83cbfe494060ea290e8d77ee7c3707298ce2
lolesterrr <kajjabwangulester@gmail.com>
2026-03-02 14:21:35 +0300

    javascript objects challenge 1

--- section 18/javascript objects.js
+++ section 18/javascript objects.js
@@ -2,5 +2,14 @@
         name: "Gonslag Tims",
         age: 60,
         hasWorkPermit: true,
-        languages:["English", "Japanese"]
+        languages:["English", "Japanese"],
+        yearsOfExperience: 40
     } 
+
+    function HouseKeeper (name, age, hasWorkPermit, languages, yearsOfExperience){
+        this.name = name;
+        this.age = age;
+        this.hasWorkPermit = hasWorkPermit;
+        this.languages = languages;
+        this.yearsOfExperience = yearsOfExperience;
+    }

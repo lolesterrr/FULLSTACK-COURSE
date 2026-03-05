6d529cfeb271a9b3bda2ff4819ff70f25ce5be45
lolesterrr <kajjabwangulester@gmail.com>
2026-03-05 10:56:40 +0300

    add event listeners

--- section 19/jQuery/index.js
+++ section 19/jQuery/index.js
@@ -5,3 +5,14 @@ console.log($("h1").css("font-family"))
 $("h1").addClass("big-title")
 
 console.log($("h1").attr("style"))
+
+
+$("h1").click(function(){
+    $("h1").css("color", "blue");
+})
+
+
+
+$("button").click(function(){
+    $("h1").css("color", "purple");
+})

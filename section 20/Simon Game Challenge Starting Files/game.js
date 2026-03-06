f75e62971b6761a31e3d0fd059b5a423e9ff39ff
lolesterrr <kajjabwangulester@gmail.com>
2026-03-06 16:54:51 +0300

    challenge 2 simon game

--- section 20/Simon Game Challenge Starting Files/game.js
+++ section 20/Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,13 @@
 // alert("the game is loaded")
+const gamePattern = [];
+
+const buttonColors = ["red","blue","green","yellow"];
+
+function nextSequence(){
+   var randomNumber = Math.floor((Math.random()*4));
+    return randomNumber
+}
+
+var randomChosenColor = buttonColors[nextSequence()];
+
+gamePattern.push(randomChosenColor)

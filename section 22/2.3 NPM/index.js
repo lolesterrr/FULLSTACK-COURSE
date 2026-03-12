45a62eb1798a52c9f1d6b42a9acf727e0b5a416e
lolesterrr <kajjabwangulester@gmail.com>
2026-03-12 12:10:01 +0300

    correction

--- section 22/2.3 NPM/index.js
+++ section 22/2.3 NPM/index.js
@@ -3,5 +3,5 @@ import generateName from "sillyname";
 import {randomSuperhero} from "superheroes"
 var sillyName = generateName();
 
-console.log(`My name is ${sillyName}.`);
+console.log(`i go by the names ${sillyName}.`);
 console.log(`I am ${randomSuperhero()}`);

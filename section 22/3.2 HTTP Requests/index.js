51ea2ae62ac80147dd636f302be1cc6e6b3f66aa
lolesterrr <kajjabwangulester@gmail.com>
2026-03-15 20:41:26 +0300

    new variable

--- section 22/3.2 HTTP Requests/index.js
+++ section 22/3.2 HTTP Requests/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 const app = express();
+const port = 8000;
 
 app.get("/", (req, res) => {
     res.send(req);
 })
 
-app.listen(8000, () => {
+app.listen(port, () => {
     console.log("Listening on Port 3000")
 })

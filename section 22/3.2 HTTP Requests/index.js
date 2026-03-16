d9c20801aca2b979f1e02e027c6cbc71c3781607
lolesterrr <kajjabwangulester@gmail.com>
2026-03-16 17:52:36 +0300

    http requests

--- section 22/3.2 HTTP Requests/index.js
+++ section 22/3.2 HTTP Requests/index.js
@@ -4,9 +4,15 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-    res.send(req);
+    res.send("Hello World!");
+})
+app.get("/contact", (req, res) => {
+    res.send("<h2>+256-769-055-233</h2>");
+})
+app.get("/about", (req, res) => {
+    res.send("<em>kajjabwangulester@2026, Japan ,Osaka</em>");
 })
 
 app.listen(port, () => {
-    console.log("Listening on Port 3000")
+    console.log("Listening on Port 8000")
 })
